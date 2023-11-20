4f557cd433ac665af98e02933719d2739eacac9a
AgusITB <agustin.garibotto.7e6@itb.cat>
2023-11-20 17:46:14 +0100

    enemy despawn

--- M1702R1-RogueLike/Assets/Scripts/Enemy.cs
+++ M1702R1-RogueLike/Assets/Scripts/Enemy.cs
@@ -10,6 +10,7 @@ public class Enemy : Character, IDamagable
     SpriteRenderer spriteRenderer;
     Color defaultColor;
     Animator enemyAnimator;
+    Color dieColor;
 
     private void Awake()
     {
@@ -17,6 +18,7 @@ public class Enemy : Character, IDamagable
         healthBar = GetComponentInChildren<EnemyHealthBar>();
         enemyAnimator = GetComponent<Animator>();
 
+        dieColor = defaultColor;
         maxHp = 15;
         speed = 2;
         currentHp = maxHp;
@@ -25,34 +27,47 @@ public class Enemy : Character, IDamagable
     public void TakeDamage(int damage)
     {
         currentHp -= damage;
-        if (currentHp <= 0) currentHp = 0; 
+        if (currentHp <= 0) currentHp = 0;
 
         Debug.Log($"I took damage, my hp is :{currentHp}");
 
-        healthBar.UpdateHealthBar(currentHp,maxHp);
+        healthBar.UpdateHealthBar(currentHp, maxHp);
 
         if (currentHp <= 0) Die();
 
     }
     public void AnimateHit()
     {
-        spriteRenderer.color = Color.red;
+      
         StartCoroutine(Flash());
-        
+        spriteRenderer.color = Color.red;
     }
     public void Die()
     {
         enemyAnimator.SetBool("isDead", true);
-        //this.gameObject.SetActive(false);
+        dieColor.a = 0;
+        StartCoroutine(Despawn(defaultColor, dieColor, 1f));
+
     }
-    
     public IEnumerator Flash()
     {
-       yield return new WaitForSeconds(0.2f);
-       spriteRenderer.color = defaultColor;
+        yield return new WaitForSeconds(0.2f);
+        spriteRenderer.color = defaultColor;
     }
 
+    IEnumerator Despawn(Color start, Color end, float duration)
+    {
+        for (float t = 0f; t < duration; t += Time.deltaTime)
+        {
+            float normalizedTime = t / duration;
+            //right here, you can now use normalizedTime as the third parameter in any Lerp from start to end
+            spriteRenderer.color = Color.Lerp(start, end, normalizedTime);
+            yield return null;
+        }
+        spriteRenderer.color = end; //without this, the value will end at something like 0.9992367
 
+        this.gameObject.SetActive(false);
+    }
 
 
 }
