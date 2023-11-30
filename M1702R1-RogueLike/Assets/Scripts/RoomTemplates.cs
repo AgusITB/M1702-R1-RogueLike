f40c79814ed6c80d327f3e9b1933a0a6a7b419c8
AgusITB <agustin.garibotto.7e6@itb.cat>
2023-11-30 20:06:12 +0100

    room spawner started

---
+++ M1702R1-RogueLike/Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+
+    public GameObject[] southRooms;
+    public GameObject[] westRooms;
+    public GameObject[] northRooms;
+    public GameObject[] eastRooms;
+     
+}
