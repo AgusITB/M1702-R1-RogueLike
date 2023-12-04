c2563c9175546d54f937d96214f633b57cdeb455
Samuel-Arteaga <samuel.arteaga.7e6@itb.cat>
2023-12-04 12:54:13 +0100

    Update script Bullet Cañon

---
+++ M1702R1-RogueLike/Assets/EnemyBulletCañon.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class EnemyBulletCañon : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    private float force=5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rotation = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotation+90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- M1702R1-RogueLike/Assets/Scripts/CañonController.cs
+++ M1702R1-RogueLike/Assets/Scripts/CañonController.cs
@@ -4,37 +4,60 @@ using UnityEngine;
 
 public class CañonController : MonoBehaviour
 {
-    [SerializeField] private Transform Player;
+    //[SerializeField] private Transform Player;
   
-    public float fireRate = 2f;  // Frecuencia de disparo en segundos
-    private float nextFireTime;
-    public EnergyBall bulletPrefab;
+    private float fireRate = 1f;  // Frecuencia de disparo en segundos
+    //private float nextFireTime;
 
+    public GameObject bullet;
     public Transform bulletDirection;
-
+    private float timer;
+    private GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     private void Update()
     {
-        if (Time.time >= nextFireTime)
+        //if (Time.time >= nextFireTime)
+        //{
+        //    //EnergyBall g = Instantiate(bulletPrefab, bulletDirection.transform.position, bulletDirection.rotation);
+        //    // Asegúrate de que BulletPool.Instance no es nulo
+        //    if (BulletPool.Instance != null)
+        //    {
+        //        EnergyBall bullet = BulletPool.Instance.GetBullet();
+
+        //        if (bullet != null)
+        //        {
+        //            bullet.transform.position = transform.position;
+        //            bullet.SetDirection(Player.transform.position);
+        //            bullet.gameObject.SetActive(true);
+
+        //            nextFireTime = Time.time + 1f / fireRate;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("BulletPool.Instance is null. Make sure it's properly initialized.");
+        //    }
+        //}
+
+        float distance= Vector2.Distance(transform.position,player.transform.position);
+        Debug.Log(distance);
+        if(distance < 10)
         {
-            //EnergyBall g = Instantiate(bulletPrefab, bulletDirection.transform.position, bulletDirection.rotation);
-            // Asegúrate de que BulletPool.Instance no es nulo
-            if (BulletPool.Instance != null)
+            timer += Time.deltaTime;
+            if (timer > fireRate)
             {
-                EnergyBall bullet = BulletPool.Instance.GetBullet();
-
-                if (bullet != null)
-                {
-                    bullet.transform.position = transform.position;
-                    bullet.SetDirection(Player.transform.position);
-                    bullet.gameObject.SetActive(true);
-
-                    nextFireTime = Time.time + 1f / fireRate;
-                }
+                timer = 0;
+                shoot();
             }
-            else
-            {
-                Debug.LogError("BulletPool.Instance is null. Make sure it's properly initialized.");
-            }
+
         }
     }
+    void shoot()
+    {
+        Instantiate(bullet,bulletDirection.position,Quaternion.identity);
+    }
+
 }
