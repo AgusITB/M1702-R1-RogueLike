311d6d130dc6aebc714618508ef11cf5605193fe
Samuel-Arteaga <samuel.arteaga.7e6@itb.cat>
2024-01-17 19:25:32 +0100

    Dialogos simples Realizados. (falta probar con opciones)

---
+++ M1702R1-RogueLike/Assets/Scripts/Dialogue/ControladorPreguntas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorPreguntas: MonoBehaviour
+{
+    //[SerializeField]
+    //private GameObject buttonPref;
+    //[SerializeField]
+    //private TextMeshProUGUI pregText;
+    //[SerializeField]
+    //private Transform opcionesContainer;
+    //private List<Button> poolButtons = new List<Button>();
+
+    //public void ActivarBotones(int cantidad, string title, Opciones[] opciones)
+    //{
+    //    pregText.text = title;
+    //    if (poolButtons.Count >= cantidad)
+    //    {
+    //        for (int i = 0; i < poolButtons.Count; i++)
+    //        {
+    //            if (i < cantidad)
+    //            {
+    //                poolButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = opciones[i].opcion;
+    //                poolButtons[i].onClick.RemoveAllListeners();
+    //                Dialogo dialogo = opciones[i].Resultante;
+    //                poolButtons[i].onClick.AddListener(() => DarFuncionAlboton(dialogo));
+    //                poolButtons[i].gameObject.SetActive(true);
+    //            }
+    //            else
+    //            {
+    //                poolButtons[i].gameObject.SetActive(false);
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        int cantidadRestante = (cantidad - poolButtons.Count);
+    //        for (int i = 0; i < cantidadRestante; i++)
+    //        {
+    //            var newButton = Instantiate(buttonPref, opcionesContainer).GetComponent<Button>();
+    //            newButton.gameObject.SetActive(true);
+    //            poolButtons.Add(newButton);
+    //        }
+    //        ActivarBotones(cantidad, title, opciones);
+    //    }
+    //}
+
+    //public void DarFuncionAlboton(Dialogo dialogo)
+    //{
+    //    DialogoManager.instance.SetConversacion(dialogo, null);
+    //}
+}
+
+
--- M1702R1-RogueLike/Assets/Scripts/Dialogue/Personaje_Dialogo.cs
+++ M1702R1-RogueLike/Assets/Scripts/Dialogue/Personaje_Dialogo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Personaje", menuName ="Sistema de Dialogo/Nuevo Personaje")]
+//[CreateAssetMenu(fileName = "Personaje", menuName ="Sistema de Dialogo/Nuevo Personaje")]
 public class Personaje_Dialogo : ScriptableObject
 {
     public string nombre;
